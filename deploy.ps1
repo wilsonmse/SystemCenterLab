5779556d7c256c5bb27a269ad195b8c671918997
Wilson Sekiguti <wilson.sekiguti@microsoft.com>
2021-03-23 22:27:31 -0300

    added dc

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 $RG = 'SystemCenterLab'
 $location = 'EASTUS2'
-$templateuri = 'https://raw.githubusercontent.com/wilsonmse/SystemCenterLab/master/main.json'
+$templateuri = 'https://raw.githubusercontent.com/wilsonmse/SystemCenterLab/SQLServer/Main.json'
 
 New-AzResourceGroup -Name $rg -Location $location #use this command when you need to create a new resource group for your deployment
 New-AzResourceGroupDeployment -ResourceGroupName $RG -TemplateUri $templateuri
