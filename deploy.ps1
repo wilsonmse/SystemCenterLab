e8662c2456114bbb83f1836008e8bf3f2b0b5da0
Wilson Sekiguti <wilson.sekiguti@microsoft.com>
2021-03-23 21:44:55 -0300

    folders renamed

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 $RG = 'SystemCenterLab'
 $location = 'EASTUS2'
-$templateuri = 'https://github.com/wilsonmse/SystemCenterLab/blob/master/active-directory-new-domain/azuredeploy.json'
+$templateuri = 'https://raw.githubusercontent.com/wilsonmse/SystemCenterLab/master/main.json'
 
 New-AzResourceGroup -Name $rg -Location $location #use this command when you need to create a new resource group for your deployment
 New-AzResourceGroupDeployment -ResourceGroupName $RG -TemplateUri $templateuri
