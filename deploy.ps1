a2cca35e3983a3db8be45b3a8d3ab29c491d1dfc
Wilson Sekiguti <wilson.sekiguti@microsoft.com>
2021-03-25 09:44:41 -0300

    s

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,8 @@
 $RG = 'SystemCenterLab'
 $location = 'EASTUS2'
 $templateuri = 'https://raw.githubusercontent.com/wilsonmse/SystemCenterLab/Orchestrator/Main.json'
+$templateparametersuri = 'https://raw.githubusercontent.com/wilsonmse/SystemCenterLab/Orchestrator/main.parameters.json'
+
 
 New-AzResourceGroup -Name $rg -Location $location #use this command when you need to create a new resource group for your deployment
-New-AzResourceGroupDeployment -ResourceGroupName $RG -TemplateUri $templateuri 
+New-AzResourceGroupDeployment -ResourceGroupName $RG -TemplateUri $templateuri -TemplateParameterUri $templateparametersuri
