875496d75b1a391eeaa822070dfd04b2cccfa31c
Wilson Sekiguti <wilson.sekiguti@microsoft.com>
2021-03-25 22:28:30 -0300

    added data disk to orchestrator

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 $RG = 'SystemCenterLab'
 $location = 'EASTUS2'
-$templateuri = 'https://raw.githubusercontent.com/wilsonmse/SystemCenterLab/Reestruturado/Main.json'
+$templateuri = 'https://raw.githubusercontent.com/wilsonmse/SystemCenterLab/master/Main.json'
 
 New-AzResourceGroup -Name $rg -Location $location #use this command when you need to create a new resources group for your deployment
 New-AzResourceGroupDeployment -ResourceGroupName $RG -TemplateUri $templateuri 
