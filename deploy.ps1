be41ec763f7c4fe8b4aad62bd20c3d0ea7e671b7
Wilson Sekiguti <wilson.sekiguti@microsoft.com>
2021-03-25 22:29:02 -0300

    deploy fixed

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 $RG = 'SystemCenterLab'
 $location = 'EASTUS2'
-$templateuri = 'https://raw.githubusercontent.com/wilsonmse/SystemCenterLab/master/Main.json'
+$templateuri = 'https://raw.githubusercontent.com/wilsonmse/SystemCenterLab/orch-instalation/Main.json'
 
 New-AzResourceGroup -Name $rg -Location $location #use this command when you need to create a new resources group for your deployment
 New-AzResourceGroupDeployment -ResourceGroupName $RG -TemplateUri $templateuri 
